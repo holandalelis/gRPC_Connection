1c2c793f266230c7eadc30ab2bad07d7951fe58a
Pedro Holanda Lelis <pedro.lelis@fcxlabs.com.br>
2024-06-26 09:54:20 -0300

    sobe a conexão entre o gRPC para o dummy

---
+++ client/Program.cs
@@ -0,0 +1,25 @@
+﻿using Dummy;
+using Grpc.Core;
+
+namespace client
+{
+    class Program
+    {
+        const string target = "127.0.0.1:50051";
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+
+            channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                    Console.WriteLine("The client connected successfully");
+            });
+            var client = new DummyService.DummyServiceClient(channel);
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,35 @@
+﻿using Grpc.Core;
+
+namespace server
+{
+    class Program
+    {
+        const int Port = 50051;
+        static void Main(string[] args)
+        {
+            Server server = null;
+
+            try
+            {
+                server = new Server
+                {
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+                Console.WriteLine("The server is listening on the port: " + Port);
+                Console.ReadKey();
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine("The server failed to start due to the following error: " + ex.Message);
+                throw;
+            }
+            finally
+            {
+                if(server != null){
+                    server.ShutdownAsync().Wait();
+                }
+            }
+        }
+    }
+}
